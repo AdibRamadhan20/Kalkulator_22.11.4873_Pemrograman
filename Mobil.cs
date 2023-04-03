08a31229e12405dd1dc9736a9ea871bffe084706
AdibRamadhan20 <127828310+AdibRamadhan20@users.noreply.github.com>
2023-04-03 22:11:18 +0700

    Add files via upload

---
+++ Mobil.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPemrogramanPraktikum4
+{
+	internal class Mobil
+	{
+		// field
+		private string _WarnaMobil;
+		private int _JumlahPintuMobil;
+		private string _MerekMobil;
+		private string _ModelMobil;
+		private int _TahunKeluaranMobil;
+		
+		// property
+		public string Warna
+		{
+			get
+			{
+				return _WarnaMobil;
+			}
+			set
+			{
+				_WarnaMobil = value;
+			}
+		}
+
+		public int JumlahPintu
+		{
+			get
+			{
+				return _JumlahPintuMobil;
+			}
+			set
+			{
+				_JumlahPintuMobil = value;
+			}
+		}
+
+		public string Merek
+		{
+			get
+			{
+				return _MerekMobil;
+			}
+			set
+			{
+				_MerekMobil = value;
+			}
+		}
+
+		public string Model
+		{
+			get
+			{
+				return _ModelMobil;
+			}
+			set
+			{
+				_ModelMobil = value;
+			}
+		}
+
+		public int TahunKeluaran
+		{
+			get
+			{
+				return _TahunKeluaranMobil;
+			}
+			set
+			{
+				_TahunKeluaranMobil = value;
+			}
+		}
+
+
+
+
+		// method
+		public void PropertyMobil()
+		{
+			Console.WriteLine("Warna          : {0}", Warna);
+			Console.WriteLine("Jumlah Pintu   : {0}", JumlahPintu);
+			Console.WriteLine("Merek          : {0}", Merek);
+			Console.WriteLine("Model          : {0}", Model);
+			Console.WriteLine("Tahun Keluaran : {0}", TahunKeluaran);
+		}
+
+		public void Gas(int Kecepatan)
+		{
+			Console.WriteLine("\nMobil " + Model + " berjalan dengan kecepatan " + Kecepatan + "\n");
+		}
+
+		public void Klakson(string Suara)
+		{
+			Console.WriteLine("{0}\n", Suara);
+		}
+
+		public void TampilanInfo()
+		{
+			Console.WriteLine("Mobil saya berwarna " + Warna + ", merek " + Merek + ", model " + Model + " keluaran tahun " + TahunKeluaran + " dengan jumlah pintu sebanyak " + JumlahPintu + "");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TugasPemrogramanPraktikum4
+{
+	internal class Program
+	{
+		class program
+		{
+
+			static void Main(string[] args)
+			{
+				Mobil mobil1 = new Mobil();
+				mobil1.Warna = "Biru Tua";
+				mobil1.JumlahPintu = 4;
+				mobil1.Merek = "Toyota";
+				mobil1.Model = "Supra";
+				mobil1.TahunKeluaran = 2023;
+
+				mobil1.PropertyMobil();
+				mobil1.Gas(65);
+				mobil1.Klakson("Beep-beep");
+				mobil1.TampilanInfo();
+			}
+		}
+
+	}
+}
+
